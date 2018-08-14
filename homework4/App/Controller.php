31e8b5ad6363c7b1858aad79f53dc6218ba50dce
Владимир Сидоров <svs_13@mail.ru>
2018-08-14 14:40:43 +0300

    hw4.Edit 2

--- homework4/App/Controller.php
+++ homework4/App/Controller.php
@@ -10,7 +10,6 @@ abstract class Controller
     public function __construct()
     {
         $this->view = new View();
-        $this->view->className = static::class; // для проверки работы УДАЛИТЬ ЭТО (ключ слово поиска var_dump)
     }
 
     public function dispatch()
