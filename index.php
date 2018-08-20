d342de6e5558bf1333972645b5ac92375a72ae89
Владимир Сидоров <svs_13@mail.ru>
2018-08-20 16:25:55 +0300

    edit index.php

--- index.php
+++ index.php
@@ -18,5 +18,30 @@
             <a href="/homework2/homework2.txt" target="_blank">Ответы и пояснения.</a>
         </p>
 
+        <p>Урок 3. Изоляция уровня представления<br>
+            <a href="/homework3/repeatLesson3/index.php" target="_blank">Повтор урока.</a>
+            <a href="/homework3/index.php" target="_blank">Главная страница.</a>
+            <a href="/homework3/homework3.txt" target="_blank">Комментарии.</a>
+        </p>
+
+        <p>Урок 4. Контроллеры и фронт-контроллер<br>
+            <a href="/homework4/repeatLesson4/index.php" target="_blank">Повтор урока.</a>
+            <a href="/homework4/index.php" target="_blank">Главная страница.</a>
+            <a href="/homework4/homework4.txt" target="_blank">Комментарии.</a>
+        </p>
+
+        <p>Урок 5. Исключения<br>
+            <a href="/homework5/repeatLesson5/index.php" target="_blank">Повтор урока.</a>
+            <a href="/homework5/index.php" target="_blank">Главная страница.</a>
+            <a href="/homework5/homework5.txt" target="_blank">Комментарии.</a>
+        </p>
+
+        <p>Урок 6. Современные стандарты PHP<br>
+            <a href="/homework6/repeatLesson6/index.php" target="_blank">Повтор урока.</a>
+            <a href="/homework6/index.php" target="_blank">Главная страница.</a>
+            <a href="/homework6/homework6.txt" target="_blank">Комментарии.</a>
+            <a href="/homework6Composer/" target="_blank">Пакет Composer</a>
+            <a href="/homework6ComposerTest/test.php" target="_blank">Тест пакета</a>
+        </p>
     </body>
 </html>
