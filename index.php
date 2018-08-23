690c9bf8dc299ad5a17c5ef51b233899c896c1b1
Владимир Сидоров <svs_13@mail.ru>
2018-08-23 14:33:53 +0300

    edit index.php

--- index.php
+++ index.php
@@ -7,41 +7,39 @@
         <h2>Ссылки на файлы домашней работы:</h2>
 
         <p>Урок 1. Модели данных и ООП<br>
-            <a href="/homework1/repeatLesson1/index.php" target="_blank">Повтор урока.</a>
+            <a href="/repeatLesson1/index.php" target="_blank">Повтор урока.</a>
             <a href="/homework1/index.php" target="_blank">Главная страница.</a>
             <a href="/homework1/homework1.txt" target="_blank">Ответы и пояснения.</a>
         </p>
 
         <p>Урок 2. Продолжение разговора о моделях<br>
-            <a href="/homework2/repeatLesson2/index.php" target="_blank">Повтор урока.</a>
+            <a href="/repeatLesson2/index.php" target="_blank">Повтор урока.</a>
             <a href="/homework2/index.php" target="_blank">Главная страница.</a>
             <a href="/homework2/homework2.txt" target="_blank">Ответы и пояснения.</a>
         </p>
 
         <p>Урок 3. Изоляция уровня представления<br>
-            <a href="/homework3/repeatLesson3/index.php" target="_blank">Повтор урока.</a>
+            <a href="/repeatLesson3/index.php" target="_blank">Повтор урока.</a>
             <a href="/homework3/index.php" target="_blank">Главная страница.</a>
             <a href="/homework3/homework3.txt" target="_blank">Комментарии.</a>
         </p>
 
         <p>Урок 4. Контроллеры и фронт-контроллер<br>
-            <a href="/homework4/repeatLesson4/index.php" target="_blank">Повтор урока.</a>
+            <a href="/repeatLesson4/index.php" target="_blank">Повтор урока.</a>
             <a href="/homework4/index.php" target="_blank">Главная страница.</a>
             <a href="/homework4/homework4.txt" target="_blank">Комментарии.</a>
         </p>
 
         <p>Урок 5. Исключения<br>
-            <a href="/homework5/repeatLesson5/index.php" target="_blank">Повтор урока.</a>
+            <a href="/repeatLesson5/index.php" target="_blank">Повтор урока.</a>
             <a href="/homework5/index.php" target="_blank">Главная страница.</a>
             <a href="/homework5/homework5.txt" target="_blank">Комментарии.</a>
         </p>
 
         <p>Урок 6. Современные стандарты PHP<br>
-            <a href="/homework6/repeatLesson6/index.php" target="_blank">Повтор урока.</a>
+            <a href="/repeatLesson6/index.php" target="_blank">Повтор урока.</a>
             <a href="/homework6/index.php" target="_blank">Главная страница.</a>
             <a href="/homework6/homework6.txt" target="_blank">Комментарии.</a>
-            <a href="/homework6Composer/" target="_blank">Пакет Composer</a>
-            <a href="/homework6ComposerTest/test.php" target="_blank">Тест пакета</a>
         </p>
     </body>
 </html>
